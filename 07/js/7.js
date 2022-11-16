4b2321ed724b466356fec6e621a10f4bd4641c56
Admin <kavyak6999@gmail.com>
2022-11-16 22:10:51 +0530

    Adding js 7

---
+++ 07/js/7.js
@@ -0,0 +1,11 @@
+var x=prompt("Enter the boat speed");
+x=parseInt(x);
+var y=prompt("Enter the stream speed");
+y=parseInt(y);
+var downstreamSpeed;
+downstreamSpeed=x+y;
+var z=prompt("Enter the Distance")
+z=parseInt(z);
+var time;
+time=z/downstreamSpeed;
+alert (time);
