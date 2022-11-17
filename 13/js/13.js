9a8518ed59c1f499cb62927876945750829db538
Admin <kavyak6999@gmail.com>
2022-11-17 14:23:54 +0530

    Adding js 13

---
+++ 13/js/13.js
@@ -0,0 +1,9 @@
+var p = prompt("Enter the value of P");
+p = parseInt(p);
+var r = prompt("Enter the value of R");
+r = parseInt(r);
+var n = prompt("Enter the value of N");
+n = parseInt(n);
+var z;
+z = p*(1-(r/100))**n
+alert(z);
