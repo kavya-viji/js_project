b11f8a9dabb654ba20e81f65a27b1a078e12d7f8
Admin <kavyak6999@gmail.com>
2022-11-17 14:28:36 +0530

    Adding js 18

---
+++ 18/js/18.js
@@ -0,0 +1,9 @@
+var name = prompt("Enter the Name");
+var a = parseInt(prompt("Enter the Tamil Mark"));
+var b = parseInt(prompt("Enter the English Mark"));
+var c = parseInt(prompt("Enter the Maths Mark"));
+var d = parseInt(prompt("Enter the Science Mark"));
+var e = parseInt(prompt("Enter the Social Mark"));
+var x = a+b+c+d+e
+var y =x/5
+alert (y)
