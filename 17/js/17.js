37fe0f5d7d8a9cc41c43bb01a403dce0302a662e
Admin <kavyak6999@gmail.com>
2022-11-17 14:28:10 +0530

    Adding js 17

---
+++ 17/js/17.js
@@ -0,0 +1,8 @@
+var m = parseInt (prompt ("Enter the value"));
+var n = parseInt (prompt ("Enter the value"));
+var o = parseInt (prompt ("Enter the value"));
+var x;
+x = (n/100)*o;
+var y;
+y = x/m;
+alert (y);
