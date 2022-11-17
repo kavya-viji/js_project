b79748aa9f1d402ffd124ca771acdd87babaee4e
Admin <kavyak6999@gmail.com>
2022-11-17 14:29:28 +0530

    Adding js 19

---
+++ 19/js/19.js
@@ -0,0 +1,15 @@
+var a = parseInt(prompt("Enter the value"));
+var b = parseInt(prompt("Enter the value"));
+var c = parseInt(prompt("Enter the  value"));
+if ((a>=b)&&(a>=c))
+{
+	alert("a is greater");
+}
+else if ((b>=a)&&(b>=c))
+{
+	alert("b is greater");
+}
+else 
+{
+	alert("c is greater");
+}
