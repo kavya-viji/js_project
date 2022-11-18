0e51bba40f7ddba9e47a2c5e1ea4b209ab12b1a1
Admin <kavyak6999@gmail.com>
2022-11-18 21:39:54 +0530

    Adding js 29

---
+++ 29/js/29.js
@@ -0,0 +1,23 @@
+// var a = ["carrot","tomato","beetroot","raddish","onion"];
+// a.splice(5,2,"brinjal","capsicum")
+// alert (a)
+
+// var a = ["carrot","tomato","beetroot","raddish","onion"];
+//b = a.splice(5,2,"brinjal","capsicum")
+//alert (b)
+
+//var a = ["carrot","tomato","beetroot","raddish","onion"];
+//a.splice(2,2)
+//alert (a)
+
+//var a = ["carrot","tomato","beetroot","raddish","onion"];
+//b = a.splice(2,2)
+//alert (b)
+
+ //var a = ["carrot","tomato","beetroot","raddish","onion"];
+// a.splice(3,2,"brinjal","capsicum")
+ //alert (a)
+
+var a = ["carrot","tomato","beetroot","raddish","onion"];
+b = a.splice(3,2,"brinjal","capsicum")
+alert (b)	
