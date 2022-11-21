eb298b87ed22e456eebeb535330d5e21e441bae7
Admin <kavyak6999@gmail.com>
2022-11-21 10:12:55 +0530

    Adding js 48

---
+++ 48/js/48.js
@@ -0,0 +1,24 @@
+var x = parseInt(prompt("Enter the percentage"));
+if(x >90)
+{
+  alert ("Grade A")
+}
+else if(x>80)
+{
+  alert ("Grade B")
+
+}
+else if(x>70)
+{
+  alert ("Grade C")
+
+}
+else if(x>60)
+{
+  alert ("Grade D")
+
+}
+else
+{
+  alert ("Grade E")
+}
